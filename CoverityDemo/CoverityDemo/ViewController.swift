c687496d9faf50f43756050e14c6f738c46369a7
Prabaharan <prabaharan.t@changepond.com>
2021-07-16 20:04:07 +0530

    Initial commit

---
+++ CoverityDemo/CoverityDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CoverityDemo
+//
+//  Created by Prabaharan Thangavel on 16/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.testFunction()
+    }
+    func testFunction() {
+        print("Coverity Demo project.")
+    }
+
+
+}
+
